aabf943b39ce51dcab9d30116d6481d2aa0ccce1
Sergey Ponomarev <stokito@gmail.com>
2019-06-30 21:09:34 +0300

    update

--- src/main/java/com/adyen/sample/GetAccounts.java
+++ src/main/java/com/adyen/sample/GetAccounts.java
@@ -5,6 +5,75 @@ import com.adyen.model.marketpay.GetAccountHolderRequest;
 import com.adyen.model.marketpay.GetAccountHolderResponse;
 import com.adyen.service.Account;
 
+/*
+class GetAccountHolderResponse {
+    verification: KYCVerificationResult{shareholders=[], accountHolder=null, bankAccounts=[]}
+    submittedAsync: false
+    accountHolderCode: Dodax5-1558533009313
+    requirementsForNextAccountState: []
+    accountHolderDetails: class AccountHolderDetails {
+        metadata: {}
+        address: null
+        phoneNumber: null
+        bankAccountDetails: []
+        individualDetails: null
+        webAddress: null
+        merchantCategoryCode: 7999
+        fullPhoneNumber: null
+        businessDetails: class BusinessDetails {
+            shareholders: []
+            doingBusinessAs: companyName
+            taxId: null
+            legalBusinessName: companyName
+            registrationNumber: companyRegistrationNumber
+        }
+        email: null@example.com
+    }
+    accountHolderStatus: class AccountHolderStatus {
+        processingState: class AccountProcessingState {
+            disableReason: null
+            processedTo: class Amount {
+                value: 0
+                currency: EUR
+            }
+            processedFrom: class Amount {
+                value: 0
+                currency: EUR
+            }
+            tierNumber: 0
+            disabled: false
+        }
+        statusReason: null
+        payoutState: class AccountPayoutState {
+            disableReason: null
+            allowPayout: true
+            tierNumber: 0
+            disabled: false
+            payoutLimit: class Amount {
+                value: 0
+                currency: EUR
+            }
+        }
+        status: Active
+        accountEvents: []
+    }
+    accounts: [class Account {
+        accountCode: 145389541
+        beneficiaryAccount: null
+        beneficiaryMerchantReference: null
+        description: MICGZQIK
+        payoutSchedule: class PayoutScheduleResponse {
+            schedule: HOLD
+            nextScheduledPayout: null
+        }
+        status: Active
+    }]
+    description: null
+    pspReference: 8515619128234340
+    legalEntity: Business
+    primaryCurrency: null
+}
+ */
 public class GetAccounts {
 
   public static void main(String[] args) throws Exception {
